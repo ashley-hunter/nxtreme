55335769d9f5c439ad0aed79ab65bd69679e559a
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2022-08-16 23:08:06 +0100

    Update executor.ts

--- packages/scss/src/executors/build/executor.ts
+++ packages/scss/src/executors/build/executor.ts
@@ -5,34 +5,50 @@ import { compile } from 'sass';
 import { BuildExecutorSchema } from './schema';
 
 export default async function buildExecutor(options: BuildExecutorSchema) {
-  const result = compile(options.entryFile, {
-    style: options.optimization ? 'compressed' : 'expanded',
+  await compileStylesheet(options.entryFile, options.outputPath, {
     sourceMap: options.sourceMap,
   });
 
-  const outputFile = join(
-    options.outputPath,
-    basename(options.entryFile, '.scss') + '.css'
-  );
-  const sourceMapFile = outputFile + '.map';
-
-  await ensureDir(options.outputPath);
-  await writeFile(
-    outputFile,
-    result.css +
-      (options.sourceMap
-        ? `/*# sourceMappingURL=${basename(
-            options.entryFile,
-            '.scss'
-          )}.css.map */`
-        : '')
-  );
-
-  if (options.sourceMap && result.sourceMap) {
-    await writeFile(sourceMapFile, JSON.stringify(result.sourceMap));
+  if (options.optimization) {
+    await compileStylesheet(options.entryFile, options.outputPath, {
+      sourceMap: options.sourceMap,
+      minify: true,
+    });
   }
 
   return {
     success: true,
   };
+}
+
+async function compileStylesheet(
+  entryFile: string,
+  outputPath: string,
+  options: CompileOptions
+): Promise<void> {
+  const result = compile(entryFile, {
+    style: options.minify ? 'compressed' : 'expanded',
+    sourceMap: options.sourceMap,
+  });
+
+  const filename = basename(entryFile, '.scss');
+
+  const outputFile = join(outputPath, filename + options.minify ? '.min.css' : '.css');
+  const sourceMapFile = outputFile + '.map';
+
+  await ensureDir(outputPath);
+
+  await writeFile(
+    outputFile,
+    result.css + (options.sourceMap ? `/*# sourceMappingURL=${sourceMapFile} */` : '')
+  );
+
+  if (options.sourceMap && result.sourceMap) {
+    await writeFile(sourceMapFile, JSON.stringify(result.sourceMap));
+  }
+}
+
+interface CompileOptions {
+  sourceMap?: boolean;
+  minify?: boolean;
 }
